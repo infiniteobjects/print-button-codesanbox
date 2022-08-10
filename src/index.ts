adb9abd54a3f65663e15f80b63839b43564bb922
matthewkeil <me@matthewkeil.com>
2022-08-10 10:14:21 -0600

    obscure api and asset-url

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+type IoPrintButton = HTMLElement & { assetUrl: string };
+
+const SCRIPT_URL = process.env.SCRIPT_URL ?? "";
+const ASSET_URL = process.env.ASSET_URL ?? "";
+
+const script = document.createElement("script");
+script.type = "text/javascript";
+script.src = SCRIPT_URL;
+
+const head = document.getElementsByTagName("head")[0];
+head.appendChild(script);
+
+const body = document.getElementsByTagName("body")[0];
+const printButton = document.createElement("io-print-button") as IoPrintButton;
+printButton.assetUrl = ASSET_URL;
+
+body.appendChild(printButton);
